dbaea62c7c1ce0554f82bb4d4bd26f881bd6b7f8
Soujanya Mirajkar <soujanyamirajkar@gmail.com>
2026-01-21 13:43:29 +0530

    lift cart up and share it between pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,18 +1,27 @@
 
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css'
 import { HomePage } from './pages/HomePage';
 //import { HomePage } from './pages/HomePage';
 import { CheckOutPage } from './pages/CheckOutPage';
 import { OrdersPage } from './pages/OrdersPage';
+import { useEffect } from 'react';
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/cart-items')
+      .then((response) => setCart(response.data));
+  }, [])
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage />}></Route>
-        <Route path='checkout' element={<CheckOutPage />}></Route>
+        <Route path='/' element={<HomePage cart={cart} />}></Route>
+        <Route path='checkout' element={<CheckOutPage cart={cart} />}></Route>
         <Route path='orders' element={<OrdersPage />}></Route>
       </Routes>
     </>
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -5,17 +5,16 @@ import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products';
 
-export function HomePage() {
+export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
+
 
   useEffect(() => {
     axios.get('/api/products')
       .then((response) => setProducts(response.data));
 
 
-    axios.get('/api/cart-items')
-      .then((response) => setCart(response.data));
+
   }, []);
 
 
