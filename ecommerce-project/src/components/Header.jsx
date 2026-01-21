6920de0936d6fbcb02e743615b2185969e2f64d0
Soujanya Mirajkar <soujanyamirajkar@gmail.com>
2026-01-21 13:15:00 +0530

    get cart from backend and display quantity

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -1,6 +1,12 @@
 import './header.css';
 import { Link } from 'react-router-dom'
-export function Header() {
+export function Header({ cart }) {
+
+    let totalQuantity = 0;
+
+    cart.forEach((item) => {
+        totalQuantity += item.quantity;
+    });
     return (
         <div className="header">
             <div className="left-section">
@@ -28,7 +34,7 @@ export function Header() {
 
                 <Link to="/checkout" className="cart-link header-link">
                     <img className="cart-icon" src="images/icons/cart-icon.png" />
-                    <div className="cart-quantity">3</div>
+                    <div className="cart-quantity">{totalQuantity}</div>
                     <div className="cart-text">Cart</div>
                 </Link>
             </div>
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,18 +1,29 @@
 
 import './HomePage.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products';
 
 export function HomePage() {
-  fetch('http://localhost:3000/api/products')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/products')
+      .then((response) => setProducts(response.data));
+
+
+    axios.get('http://localhost:3000/api/cart-items')
+      .then((response) => setCart(response.data));
+  }, []);
+
 
 
   return (
     <>
       <title>E-commerce project</title>
-      <Header />
+      <Header cart={cart} />
       <div className="home-page">
         <div className="products-grid">
           {products.map((product) => (
